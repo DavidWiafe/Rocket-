c1f82985b31410afac8b851de2c2268f7c234725
DavidWiafe <wiafe.d@hotmail.com>
2019-01-02 19:14:43 -0500

    first commit

---
+++ Rocket-Ship/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Rocket_Ship
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.status = new System.Windows.Forms.ProgressBar();
+            this.rocket = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Rockect_Ship = new System.Windows.Forms.GroupBox();
+            this.Lanuch_BTN = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.counter = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.Rockect_Ship.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // status
+            // 
+            this.status.Location = new System.Drawing.Point(196, 678);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(147, 23);
+            this.status.TabIndex = 0;
+            this.status.Value = 100;
+            // 
+            // rocket
+            // 
+            this.rocket.AutoSize = true;
+            this.rocket.Location = new System.Drawing.Point(299, 545);
+            this.rocket.Name = "rocket";
+            this.rocket.Size = new System.Drawing.Size(35, 13);
+            this.rocket.TabIndex = 1;
+            this.rocket.Text = "label1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(85, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(68, 110);
+            this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Green-Star";
+            // 
+            // Rockect_Ship
+            // 
+            this.Rockect_Ship.Controls.Add(this.panel1);
+            this.Rockect_Ship.Location = new System.Drawing.Point(400, 475);
+            this.Rockect_Ship.Name = "Rockect_Ship";
+            this.Rockect_Ship.Size = new System.Drawing.Size(216, 144);
+            this.Rockect_Ship.TabIndex = 3;
+            this.Rockect_Ship.TabStop = false;
+            // 
+            // Lanuch_BTN
+            // 
+            this.Lanuch_BTN.Location = new System.Drawing.Point(49, 704);
+            this.Lanuch_BTN.Name = "Lanuch_BTN";
+            this.Lanuch_BTN.Size = new System.Drawing.Size(75, 23);
+            this.Lanuch_BTN.TabIndex = 4;
+            this.Lanuch_BTN.Text = "Launch";
+            this.Lanuch_BTN.UseVisualStyleBackColor = true;
+            this.Lanuch_BTN.Click += new System.EventHandler(this.lanuching);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(159, 394);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(12, 333);
+            this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(12, 394);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(12, 333);
+            this.panel3.TabIndex = 6;
+            // 
+            // counter
+            // 
+            this.counter.AutoSize = true;
+            this.counter.Location = new System.Drawing.Point(481, 417);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(0, 13);
+            this.counter.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 739);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Lanuch_BTN);
+            this.Controls.Add(this.Rockect_Ship);
+            this.Controls.Add(this.rocket);
+            this.Controls.Add(this.status);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.Rockect_Ship.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar status;
+        private System.Windows.Forms.Label rocket;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox Rockect_Ship;
+        private System.Windows.Forms.Button Lanuch_BTN;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label counter;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rocket-Ship/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rocket_Ship
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private static double fuelUsage;
+
+        private static int currentSpeed = 1;
+
+        private static int upKeyClickCount = 0;
+
+        //moving objects using keyboard keys
+        private void Game_KeyDown(object sender, KeyEventArgs key)
+        {
+          
+            #region movement controls
+            if (key.KeyCode == Keys.W)
+            {
+                //go up
+                //rocket.Top -= 5;
+                Rockect_Ship.Top -= currentSpeed;
+                upKeyClickCount += 1;
+            }
+            Console.WriteLine(upKeyClickCount);
+            if (key.KeyCode == Keys.S)
+            {
+                //go down
+                rocket.Top += 5;
+            }
+
+            if (key.KeyCode == Keys.A)
+            {
+                //go toward the left
+                rocket.Left -= 10;
+            }
+            if (key.KeyCode == Keys.D)
+            {
+                //go towards the right
+                rocket.Left += 10;
+            }
+
+            #endregion
+
+            #region status bar
+            if (key.KeyCode == Keys.H)
+            {
+                status.Value -= 10;
+            }
+            //every fith decrease satus bar by  
+            if ( upKeyClickCount % 5 == 0)
+            {
+                currentSpeed += 2;
+
+                status.Value -= 2;
+            }
+            #endregion
+        }
+
+
+        public void start_countDown()
+        {
+            
+            String[] counterArray = {"3 ","2 ","1... ","and we have lift off" };
+
+            for (int count = 0; count < 4; count++ ) {
+
+                counter.Text = counterArray[count];
+
+                 counter.Update();
+            }
+           
+        }
+
+        public void ascend_rocket()
+        {
+            int up = 0;
+
+            
+            for(; up < 35; up += 5 ){
+                
+                Rockect_Ship.Top -= up;
+
+               // up++;
+
+            }
+
+        }
+
+        #region button clicks
+        private void lanuching(object sender, EventArgs e) {
+
+            start_countDown();
+
+            ascend_rocket();
+
+        }
+
+        #endregion
+
+    }
+}
